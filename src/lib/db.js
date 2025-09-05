835bd25fda4559c817f0705d3b3525d146dafc7f
atchaya2015 <atchayathiru958@gmail.com>
2025-09-05 09:40:00 +0530

    file changed

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,5 +5,6 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 28399,
+  port: Number(process.env.DB_PORT) || 28399,
+  ssl: { rejectUnauthorized: true }, // Required for Aiven
 });
